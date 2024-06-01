5edc34e557df8e816f47dd6898a1f71480abdd2e
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-06-01 14:45:38 +0200

    more tests

--- pipex.c
+++ pipex.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+
+// Assuming these are the functions available in your libft
+#include "libft/include/libft.h"
 
 // Define the t_command struct for the linked list
 typedef struct s_command {
@@ -9,10 +13,85 @@ typedef struct s_command {
     struct s_command *next; // Pointer to the next command
 } t_command;
 
+void	ft_free_split(char **arr)
+{
+	int	i;
+
+	if (!arr)
+		return ;
+	i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
+}
+
+// Creates the full path for a command
+char *create_cmd_path(char *dir, char *cmd) {
+    char *path;
+    char *full_path;
+
+    path = ft_strjoin(dir, "/");
+    full_path = ft_strjoin(path, cmd);
+    free(path);
+    return (full_path);
+}
+
+// Checks if a command exists in the given paths
+char *check_command_in_paths(char **paths, char *cmd)
+{
+	char *full_path;
+	int x = 0;
+
+	while (paths[x]) {
+		full_path = create_cmd_path(paths[x], cmd);
+		if (access(full_path, X_OK) == 0) {
+			return (full_path);
+		}
+		free(full_path);
+		x++;
+	}
+	return (NULL);
+}
+
+// Finds the correct path for a command using the environment variables
+char *find_correct_path(char *cmd, char **ep) {
+    int i = 0;
+    char *path;
+    char *executable_path;
+    char **paths;
+
+    while (ep[i]) {
+        if (ft_strncmp(ep[i], "PATH=", 5) == 0) {
+            path = ep[i] + 5;
+            paths = ft_split(path, ':');
+            executable_path = check_command_in_paths(paths, cmd);
+            ft_free_split(paths);
+            if (executable_path != NULL) {
+                return (executable_path);
+            }
+            break;
+        }
+        i++;
+    }
+    return (NULL);
+}
+
 // Executes a command using execve
 void execute_command(char *cmd[], char **env) {
-    execve(cmd[0], cmd, env);
+    char *executable_path;
+
+    if (ft_strchr(cmd[0], '/') != NULL) {
+        executable_path = ft_strdup(cmd[0]);
+    } else {
+        executable_path = find_correct_path(cmd[0], env);
+        if (executable_path == NULL) {
+            fprintf(stderr, "command not found: %s\n", cmd[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    execve(executable_path, cmd, env);
     perror("execve failed");
+    free(executable_path);
     exit(EXIT_FAILURE);
 }
 
@@ -87,30 +166,41 @@ void add_command(t_command **head, char **argv) {
 
 // Populates the linked list with initial commands
 void populate_commands(t_command **head) {
-    add_command(head, (char *[]) {"/bin/sleep", "1", NULL});
-    add_command(head, (char *[]) {"/bin/ls", "-l", NULL});
-    add_command(head, (char *[]) {"/usr/bin/grep", "2023", NULL});
+    // add_command(head, (char *[]) {"sleep", "1", NULL});
+    add_command(head, (char *[]) {"ls", "-l", NULL});
+    add_command(head, (char *[]) {"/usr/bin/grep", "pipe", NULL});
     add_command(head, (char *[]) {"/usr/bin/wc", "-m", NULL});
-    add_command(head, (char *[]) {"/usr/bin/wc", "-l", NULL});
+    // add_command(head, (char *[]) {"/usr/bin/wc", "-l", NULL});
+}
+
+int count_commands(t_command *head) {
+    int count = 0;
+    t_command *current = head;
+    while (current) {
+        count++;
+        current = current->next;
+    }
+    return count;
 }
 
 int main(int argc, char **argv, char **env) {
     (void)argc; // Suppress unused parameter warning
     (void)argv; // Suppress unused parameter warning
 
-    int num_commands = 5; // Number of commands to be executed
+    // Linked list head for commands
+    t_command *commands = NULL;
+
+    // Populate the linked list with commands
+    populate_commands(&commands);
+
+    // Count the number of commands
+    int num_commands = count_commands(commands);
 
     // Array to store file descriptors for pipes
     int pipe_fds[num_commands - 1][2];
 
     // Array to store process IDs of child processes
     pid_t pids[num_commands];
-
-    // Linked list head for commands
-    t_command *commands = NULL;
-
-    // Populate the linked list with commands
-    populate_commands(&commands);
 
     // Counter variable for loops
     int i = 0;
@@ -136,8 +226,12 @@ int main(int argc, char **argv, char **env) {
             // Setup redirections for the child process
             if (i == 0) {
                 // First command: no input redirection
-                close(pipe_fds[i][0]); // Close unused read end of the pipe
-                setup_child(-1, pipe_fds[i][1], current_cmd->argv, env); // Set up output redirection to the pipe
+                if (num_commands > 1) {
+                    close(pipe_fds[i][0]); // Close unused read end of the pipe
+                    setup_child(-1, pipe_fds[i][1], current_cmd->argv, env); // Set up output redirection to the pipe
+                } else {
+                    setup_child(-1, -1, current_cmd->argv, env); // No redirection needed
+                }
             } else if (i == num_commands - 1) {
                 // Last command: no output redirection
                 close(pipe_fds[i - 1][1]); // Close unused write end of the previous pipe
