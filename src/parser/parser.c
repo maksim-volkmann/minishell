28e1e8c0ab494bc27da6d86b6841235e68883146
Adrian Hernandez <adrherna@2-H-7.42heilbronn.de>
2024-06-10 10:17:33 +0200

    merge 3

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,44 +6,74 @@
 /*   By: adrherna <adrianhdt.2001@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:11:52 by adrherna          #+#    #+#             */
-/*   Updated: 2024/06/07 12:47:14 by adrherna         ###   ########.fr       */
+/*   Updated: 2024/06/08 13:59:13 by adrherna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parser.h"
-#include <stdio.h>
 
-// void	print_redirection(t_redirection *redir)
-// {
-// 	// while (redir != NULL)
-// 	// {
-// 		printf("Type: %d, File: %s\n", redir->type, redir->file);
-// 	// }
-// }
+int	is_redirection_operator(const char *str)
+{
+	const char	*redirection_operators[] = {">", ">>", "<", "<<", NULL};
+	int			i;
 
-// void	print_command(t_command *cmd)
-// {
-// 	int	i;
+	i = 0;
+	while (redirection_operators[i] != NULL)
+	{
+		if (strcmp(str, redirection_operators[i]) == 0)
+			return (1);
+		i++;
+	}
 
-// 	if (cmd == NULL)
-// 	{
-// 		printf("Command is NULL\n");
-// 		return;
-// 	}
+	return (0);
+}
 
-// 	printf("Arguments: ");
-// 	for (i = 0; cmd->argv && cmd->argv[i]; i++)
-// 	{
-// 		printf("%s ", cmd->argv[i]);
-// 	}
-// 	printf("\n");
+int	get_size(char **src)
+{
+	int	size;
 
-// 	printf("Output redirections:\n");
-// 	print_redirection(cmd->output);
+	size = 0;
+	while (src[size] != NULL)
+		size++;
+	return (size);
+}
 
-// 	printf("Input redirections:\n");
-// 	print_redirection(cmd->input);
-// }
+
+void	assign_to_darray(char **src, char **darray, int *i, int *j)
+{
+	size_t const	len = ft_strlen(src[*i]) + 1;
+
+	darray[*j] = (char *)malloc(len * sizeof(char));
+	ft_strlcpy(darray[*j], src[*i], len);
+	(*i)++;
+	(*j)++;
+}
+
+char	**ft_clean_darray(char **src)
+{
+	int const	size = get_size(src);
+	char		**darray;
+	int			i;
+	int			j;
+
+	i = 0;
+	j = 0;
+	darray = (char **)malloc(sizeof(char *) * (size + 1));
+	while (src[i] != NULL)
+	{
+		if (is_redirection_operator(src[i]))
+		{
+			if (src[i + 1] != NULL)
+				i += 2;
+			else
+				break ;
+		}
+		else
+			assign_to_darray(src, darray, &i, &j);
+	}
+	darray[j] = NULL;
+	return (darray);
+}
 
 void	ft_parser(t_command **cmds, t_token **tokens)
 {
@@ -58,9 +88,8 @@ void	ft_parser(t_command **cmds, t_token **tokens)
 		new = ft_new_node();
 		new->argv = ft_get_darray(&current);
 		new->output = ft_fill_output(new->argv);
-		
 		new->input = ft_fill_input(new->argv);
-
+		new->argv = ft_clean_darray(new->argv);
 		add_or_init_node(cmds, new);
 		// print_command(new);
 	}
