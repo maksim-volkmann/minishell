00de7cf4797e7f77963a68c5d8bc85c19582590e
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-06-26 14:10:19 +0200

    main

--- src/minishell.c
+++ src/minishell.c
@@ -261,10 +261,26 @@ int main(int argc, char **argv, char **env)
 			free(input);
 			continue ;
 		}
+		else if (shell.cmds && ft_strcmp(shell.cmds->argv[0], "exit") == 0)
+		{
+			execute_exit(shell.cmds->argv, &shell);
+			free(input);
+			free_command(shell.cmds);
+			free_token_list(tokens);
+			break;
+		}
+		else
+		{
+			execute_commands(shell.cmds, &shell);
+		}
+
 		// PRINTING SHELL STRUCT!!!
 		// print_command_details(shell.cmds);
 		// EXECUTION
-		execute_commands(shell.cmds, &shell);
+		  // Check if the first command is "exit" and handle it directly
+
+
+
 		free_command(shell.cmds);
 		free_token_list(tokens);
 		free(input);
@@ -310,12 +326,8 @@ int main(int argc, char **argv, char **env)
 // 			free(input);
 // 			continue ;
 // 		}
-
-// 		if (is_builtin(shell.cmds->argv[0]))
-// 			execute_builtin(shell.cmds, &shell);
-
-// 		else
-// 			execute_commands(shell.cmds, shell.env_list);
+// 		//command execv
+// 		execute_commands(shell.cmds, &shell);
 // 		free_command(shell.cmds);
 // 		free_token_list(tokens);
 // 		free(input);
